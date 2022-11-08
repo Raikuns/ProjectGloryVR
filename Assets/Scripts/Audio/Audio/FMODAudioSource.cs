266cca8b2ecc15bc6c5e54777c042721b1f2f928
Joop Westra <joopwestra@gmail.com>
2022-11-08 09:42:29 +0100

    Latest art

--- Assets/Scripts/Audio/Audio/FMODAudioSource.cs
+++ Assets/Scripts/Audio/Audio/FMODAudioSource.cs
@@ -98,19 +98,19 @@ public class FMODAudioSource : FMODUnity.EventHandler
             PlayAudioFMOD();
         }
     }
-    [MenuItem("GameObject/Audio/Add Uber Source", false, 10)]
-    static void CreateCustomGameObject(MenuCommand menuCommand)
-    {
-        // Create a custom game object
-        GameObject go = new GameObject("Uber Audio Source");
-        // Ensure it gets reparented if this was a context click (otherwise does nothing)
-        GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
-        // Register the creation in the undo system
-        Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
-        Selection.activeObject = go;
+    //[MenuItem("GameObject/Audio/Add Uber Source", false, 10)]
+    //static void CreateCustomGameObject(MenuCommand menuCommand)
+    //{
+    //    // Create a custom game object
+    //    GameObject go = new GameObject("Uber Audio Source");
+    //    // Ensure it gets reparented if this was a context click (otherwise does nothing)
+    //    GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
+    //    // Register the creation in the undo system
+    //    Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
+    //    Selection.activeObject = go;
 
-        go.AddComponent<FMODAudioSource>();
-    }
+    //    go.AddComponent<FMODAudioSource>();
+    //}
 
 
     private IEnumerator _PlayDelayed(float delay)
